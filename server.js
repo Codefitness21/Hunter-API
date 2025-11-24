87362b13fba5256f1e55a03d46e0aed322ef4cbb
Codefitness21 <kelly.m.hill2115@gmail.com>
2025-11-24 15:23:01 -0600

    added notes to server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+//Since I'm using express, it needs to be required for use.
 const express = require("express");
+//can use all the methods that came with express 
 const app = express();
+//Set up port we want it to be on.
 const PORT = 8000;
+
+//simple json object. products is the object. products is the property that has the value of the object.
 const products = {
   "less than temperature": {
     water: "Dont water when forecast temperature is less than",
@@ -20,13 +25,16 @@ const products = {
   },
 };
 
+//get is a method that comes with express and can plug stuff into this get and will have the req and res callbacks. This is a network request and fires function within the get kind of like an event listener. I want to send a file, which is index.html and have to tell it where to find this file and look for it in the current directory (dirname), which is the root and you'l find the html file. But if that's the case we need to make an index.html file. get req for the main page.
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+//Make a get request if they want to ping our server. We're going to use the /api route. When req is made to api, want to respsond with json. get request for if they type in something slightly different like /api.  
+//How we can use query parameters can ask for specific information and not get the entire object back. After the /api/: you can add a name for the query parameter, which allows me to req.params.name, when using query parameters so I'll use 'name' as an example. so if there is a word after /api/ I can grab it with this bit of code req.params.name. 
 app.get("/api/:name", (req, res) => {
   const waterProducts = req.params.name.toLowerCase()
-  
+//Can't use dot notation due to spaces so we use brackets. seeing if waterProducts exist within the products object.
   if( products[waterProducts] ){
     res.json(products[waterProducts])
   }else{
@@ -34,6 +42,7 @@ app.get("/api/:name", (req, res) => {
   }
 });
 
+//Telling the server to listen and what to do and where to listen. 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is now running on port ${PORT}!`);
 });
